486475fd27824d522d30252a08f22c4a023533d4
endevel <endevel@mail.ru>
2018-06-22 03:16:23 +0500

    add timer

--- src/NumSelector/TimerOff.jsx
+++ src/NumSelector/TimerOff.jsx
@@ -4,7 +4,6 @@ class TimerOff extends Component {
 
     constructor (props) {
         super(props);
-        this.initTimer();
     }
 
     state = {
@@ -14,81 +13,40 @@ class TimerOff extends Component {
         ss: 0
     }
 
-    initTimer = () => () => {
-        const currentDate = new Date();
-        const endDate = currentDate + 6000;
-        let seconds = 600;//endDate - currentDate;
-
-        if (seconds > 0) {
-            let minutes = seconds/60; 
-            let hours = minutes/60;
-            let days = hours / 24;
-
-            minutes = (hours - Math.floor(hours)) * 60; 
-            days = Math.floor(days);
-            hours =  Math.floor(hours)- days * 24; 
-     
-            seconds = Math.floor((minutes - Math.floor(minutes)) * 60); 
-            minutes = Math.floor(minutes); 
-     
-            this.setState ({
-                dd: days,
-                hh: hours,
-                mm: minutes,
-                ss: seconds 
-            })
-
-            //setTimePage(days, hours, minutes, econds); 
-     
-            function secOut() {
-              if (seconds == 0) { 
-                  if (minutes == 0) { 
-                      if (hours == 0) { 
-                             if(days == 0){
-                                  this.showMessage(timerId); 
-                              }
-                              else{
-                                   days--; 
-                                  hours = 24; 
-                                minutes = 59; 
-                                seconds = 59; 
-                              }
-                      }
-                      else {
-                          hours--; 
-                          minutes = 59; 
-                          seconds = 59; 
-                      }
-                  }
-                  else {
-                      minutes--; 
-                      seconds = 59; 
-                  }
-              }
-              else {
-                  seconds--; 
-              }
-                  this.setState ({
-                    dd: days,
-                    hh: hours,
-                    mm: minutes,
-                    ss: seconds 
-                })
-            }
-            var timerId = setInterval(secOut, 1000) 
-        }
-        else {
-            alert("Установленая дата уже прошла");
-        }
+    componentDidMount() {
+        this.timerId = setInterval(
+            () => this.tick(), 1000
+        );
     }
 
-    showMessage() {
-        //
+    tick() {
+        let sec = this.state.ss + 1;
+        let min = this.state.mm;
+
+        if (sec >= 60) {
+            sec = 0;
+            min = min + 1;
+        }
+
+        this.setState ({
+            ss: sec,
+            mm: min
+        })
     }
+
+    componentWillUnmount () {
+        clearInterval(this.timerId);
+    }
+
     render() {
+
+        let h = this.state.hh < 10 ? '0' + this.state.hh.toString() : this.state.hh.toString();
+        let m = this.state.mm < 10 ? '0' + this.state.mm.toString() : this.state.mm.toString();
+        let s = this.state.ss < 10 ? '0' + this.state.ss.toString() : this.state.ss.toString();
+
         return (
-            <div>
-                {this.state.mm}  :  {this.state.ss}
+            <div className = "timer-off">
+                {h} : {m}  :  {s}
             </div>
         )
     }
